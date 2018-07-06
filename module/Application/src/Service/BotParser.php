33af5076e599df49c193c3e60d005f1d4b5fa344
Pedro de Oliveira <falsovsky@gmail.com>
2018-07-06 01:06:40 +0000

    Add items, unique items and penalties to playerinfo

--- module/Application/src/Service/BotParser.php
+++ module/Application/src/Service/BotParser.php
@@ -6,6 +6,28 @@ use Carbon\Carbon;
 class BotParser
 {
     private $config;
+    private $items = [
+        'amulet',
+        'boots',
+        'charm',
+        'gloves',
+        'helm',
+        'leggings',
+        'ring',
+        'shield',
+        'tunic',
+        'weapon'
+    ];
+    private $penalties = [
+        'kick',
+        'logout',
+        'msg',
+        'nick',
+        'part',
+        'quest',
+        'quit',
+        'total',
+    ];
 
     public function __construct(array $config)
     {
@@ -75,7 +97,7 @@ class BotParser
                     continue;
                 }
 
-                $database[] = [
+                $record = [
                     'nick' => $data[0], // nick
                     'level' => $data[3], // level
                     'admin' => ($data[2] ? 'Yes' : 'No'), // admin
@@ -179,11 +201,65 @@ class BotParser
                         (int) $data[30], // sum
                     'alignment' => $this->parseAlignment($data[31]), // alignment
                 ];
+
+                foreach ($this->items as $item) {
+                    $unique = $this->getUniqueItem($record[$item]['display']);
+                    if ($unique) {
+                        $record[$item]['unique'] = $unique;
+                    }
+                }
+
+                foreach ($this->penalties as $penalty) {
+                    if ($record[$penalty.'_pen']['display'] == '1 second') {
+                        $record[$penalty.'_pen']['display'] = "None";
+                    }
+                }
+
+                $database[] = $record;
             }
             fclose($handle);
         }
 
         return ['data' => $database];
+    }
+
+    private function getUniqueItem($item_value)
+    {
+        $result = null;
+
+        $re = $re = '/\d+([a-h])/';
+        preg_match($re, $item_value, $matches);
+
+        if (isset($matches[1])) {
+            switch ($matches[1]) {
+                case 'a':
+                    $result = "Mattt's Omniscience Grand Crown";
+                    break;
+                case 'b':
+                    $result = "Res0's Protectorate Plate Mail";
+                    break;
+                case 'c':
+                    $result = "Dwyn's Storm Magic Amulet";
+                    break;
+                case 'd':
+                    $result = "Jotun's Fury Colossal Sword";
+                    break;
+                case 'e':
+                    $result = "Drdink's Cane of Blind Rage";
+                    break;
+                case 'f':
+                    $result = "Mrquick's Magical Boots of Swiftness";
+                    break;
+                case 'g':
+                    $result = "Jeff's Cluehammer of Doom";
+                    break;
+                case 'h':
+                    $result = "Juliet's Glorious Ring of Sparkliness";
+                    break;
+            }
+        }
+
+        return $result;
     }
 
     public function getPlayerInfo($nick)
