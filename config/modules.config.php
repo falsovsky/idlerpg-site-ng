33866ad440609672eead06e9e38913f897a099ef
Pedro de Oliveira <falsovsky@gmail.com>
2018-07-06 01:24:08 +0000

    remove unused modules

--- config/modules.config.php
+++ config/modules.config.php
@@ -11,9 +11,7 @@
  * This should be an array of module namespaces used in the application.
  */
 return [
-    'Zend\Mvc\I18n',
     'Zend\Router',
-    'Zend\Validator',
     'Zend\Navigation',
     'Application',
 ];
